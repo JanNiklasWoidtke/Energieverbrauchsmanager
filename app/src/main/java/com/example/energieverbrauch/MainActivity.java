a211b394e1f893554bcc8cee1d5be01c08fcc80a
JanNiklasWoidtke <52413553+JanNiklasWoidtke@users.noreply.github.com>
2019-07-15 03:06:28 +0200

    DrawerMenu funktioniert und verweist bei Click auf die jeweilgen Fragments. Funktionsweise der Fragments muss noch implementiert werden, ProgressBar in StartFragment funktioniert momentan nicht mehr, da noch nicht in StartFragement implementiert.

--- app/src/main/java/com/example/energieverbrauch/MainActivity.java
+++ app/src/main/java/com/example/energieverbrauch/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.energieverbrauch;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public ProgressBar PBcircle;
     public EditText EditTextMaxVerbrauchSoll;
     public TextView TextViewAktVerbrauch;
+    public DrawerLayout drawer;
 
     float obergrenzeVerbrauch = 0;
     float aktuellerVerbrauch = 10;
@@ -26,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) { //only switch to Start if app is started initially. Rotating the screen wont cause jumping back to start.
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new start_fragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_start);
+        }
+/*
         TextViewAktVerbrauch = findViewById(R.id.aktVerbrauch);
         TextViewAktVerbrauch.setText(aktuellerVerbrauch + " kWh");
 
@@ -57,7 +82,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+*/
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_start:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new start_fragment()).commit();
+                break;
+            case R.id.nav_MyConsumption:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyConsumption_fragment()).commit();
+                break;
+            case R.id. nav_MyCounters:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyCounters_fragment()).commit();
+                break;
+            case R.id. nav_SavingTips:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SavingTips_fragment()).commit();
+                break;
+            case R.id. nav_Settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Settings_fragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else super.onBackPressed();
     }
 
     public void verbrauchsFortschrittBerechnen() {
@@ -67,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void verbrauchsanzeigeAktualisieren() {
         PBcircle = findViewById(R.id.PBcircle);
-        if (verbrauchAnteilInt <= 100)PBcircle.setProgress(verbrauchAnteilInt);
+        if (verbrauchAnteilInt <= 100) PBcircle.setProgress(verbrauchAnteilInt);
         else PBcircle.setProgress(100);
 
     }
