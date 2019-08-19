30cfacdc6a9bd8946c776a525a4fd58cab3da869
JanNiklasWoidtke <52413553+JanNiklasWoidtke@users.noreply.github.com>
2019-08-19 23:26:21 +0200

    Tabelle mit Zahlerdaten wird korrekt dargestellt und kann aktualisiert werden, keine Bugs bekannt.

--- app/src/main/java/com/example/energieverbrauch/MainActivity.java
+++ app/src/main/java/com/example/energieverbrauch/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
